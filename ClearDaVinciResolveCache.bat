d2a89d2e059d8664d7f2a2f11c4fb42b2d2a3dc8
TheCuriosium <45500594+TheCuriosium@users.noreply.github.com>
2021-10-25 13:23:28 -0600

    Add files via upload
    Windows batch file for clearing the Black Magic Design DaVinci Resolve Optimized Media Cache.

---
+++ ClearDaVinciResolveCache.bat
@@ -0,0 +1,46 @@
+@Echo Off
+REM BEGIN CONFIGURATION
+REM IF YOU MOVE THE CACHE FILES LOCATION, YOU MUST CONFIGURE THESE TWO LINES!
+set drcachedrive=C:
+set drcachefolder=%USERPROFILE%\Videos\CacheClip
+REM END CONFIGURATION
+echo If DaVinci resolve is running, close it now!
+echo IF DAVINCI RESOLVE IS RUNNING, CLOSE IT NOW!!
+echo.
+echo Are you sure you want to delete the Davinci Resolve Optimized Media Cache!?
+choice /C YN /M "Press Y for Yes, N for No:"
+rem reecho '%errorlevel%'
+if '%errorlevel%'=='1' goto itsyes
+if '%errorlevel%'=='2' goto itsno
+echo.
+goto getout
+:itsyes
+%drcachedrive%
+cd %drcachefolder%
+dir
+rmdir OptimizedMedia /S
+dir
+goto getout
+:itsno
+rem echo No
+echo Canceled!
+:getout
+rem set
+pause
+
+REM Copyright 2021 TheCuriosium AKA Michael Aichlmayr
+
+REM Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+REM associated documentation files (the "Software"), to deal in the Software without restriction,
+REM including without limitation the rights to use, copy, modify, merge, publish, distribute,
+REM sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+REM furnished to do so, subject to the following conditions:
+
+REM The above copyright notice and this permission notice shall be included in all copies or
+REM substantial portions of the Software.
+
+REM THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+REM NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+REM NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+REM DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+REM OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
